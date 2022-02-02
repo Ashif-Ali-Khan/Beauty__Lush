659fce87e2dbfef7d9078e838fb10980b18ed39c
Ashif-Ali-Khan <85801078+Ashif-Ali-Khan@users.noreply.github.com>
2022-02-02 16:50:56 +0530

    Initial commit

---
+++ src/constants/data.js
@@ -0,0 +1,51 @@
+const wines = [
+  {
+    title: "number one",
+    price: "Rs1200",
+    tags: "Bottle"
+  }
+];
+const limes = [
+  {
+    title: "number two",
+    price: "Rs 700",
+    tags: "Bottle"
+  }
+];
+
+const cocktails = [
+  {
+    title: "number three",
+    price: "Rs 200",
+    tags: "Tube"
+  }
+];
+
+const awards = [
+  {
+    imgUrl:
+      "https://images.pexels.com/photos/807598/pexels-photo-807598.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    title: "Bib Gourmond",
+    subtitle: "Lorem ipsum dolor sit amet, consectetur."
+  },
+  {
+    imgUrl:
+      "https://images.pexels.com/photos/807598/pexels-photo-807598.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    title: "Rising Star",
+    subtitle: "Lorem ipsum dolor sit amet, consectetur."
+  },
+  {
+    imgUrl:
+      "https://images.pexels.com/photos/807598/pexels-photo-807598.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    title: "AA Hospitality",
+    subtitle: "Lorem ipsum dolor sit amet, consectetur."
+  },
+  {
+    imgUrl:
+      "https://images.pexels.com/photos/807598/pexels-photo-807598.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    title: "Outstanding Chef",
+    subtitle: "Lorem ipsum dolor sit amet, consectetur."
+  }
+];
+
+export default { wines, limes, cocktails, awards };
